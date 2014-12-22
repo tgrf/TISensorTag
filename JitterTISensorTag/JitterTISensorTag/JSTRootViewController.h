8aa7f82888bdd10724553ffe12b2e9cb3e7a2825
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2014-12-22 15:32:39 +0100

    initial project configuration;

---
+++ JitterTISensorTag/JitterTISensorTag/JSTRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  JSTRootViewController.h
+//  JitterTISensorTag
+//
+//  Created by Tomasz Grynfelder on 22/12/14.
+//  Copyright (c) 2014 mceconf.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSTRootViewController : UIViewController
+
+
+@end
+
---
+++ JitterTISensorTag/JitterTISensorTagTests/JitterTISensorTagTests.m
@@ -0,0 +1,40 @@
+//
+//  JitterTISensorTagTests.m
+//  JitterTISensorTagTests
+//
+//  Created by Tomasz Grynfelder on 22/12/14.
+//  Copyright (c) 2014 mceconf.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface JitterTISensorTagTests : XCTestCase
+
+@end
+
+@implementation JitterTISensorTagTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
