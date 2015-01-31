751c1d7ef36bd15f6de20e4ec8382f37b2e805a1
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2015-01-31 20:17:23 +0100

    blowing;

---
+++ JitterTISensorTag/JitterTISensorTag/UI/Blow/JSTBlowView.h
@@ -0,0 +1,12 @@
+//
+//  JSTBlowView.h
+//  JitterTISensorTag
+//
+//  Created by Tomasz Grynfelder on 31/01/15.
+//  ***REMOVED***
+//
+#import <Foundation/Foundation.h>
+
+@interface JSTBlowView : UIView
+@property (nonatomic, strong, readonly) UILabel *valuesLabel;
+@end
---
+++ JitterTISensorTag/JitterTISensorTag/UI/Blow/JSTBlowViewController.h
@@ -0,0 +1,11 @@
+//
+//  JSTBlowViewController.h
+//  JitterTISensorTag
+//
+//  Created by Tomasz Grynfelder on 31/01/15.
+//  ***REMOVED***
+//
+#import <Foundation/Foundation.h>
+
+@interface JSTBlowViewController : UIViewController
+@end
