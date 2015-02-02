2120966c3bdf25e5aa1cbb9419303b2a4c4ad15d
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2015-02-02 21:37:05 +0100

    details header view; wand view in progress;

--- JitterTISensorTag/JitterTISensorTag/UI/Wand/JSTWandView.h
+++ JitterTISensorTag/JitterTISensorTag/UI/Wand/JSTWandView.h
@@ -7,6 +7,9 @@
 //
 #import <Foundation/Foundation.h>
 
+@class JSTDetailsHeaderView;
+
 @interface JSTWandView : UIView
 @property (nonatomic, strong, readonly) UILabel *valuesLabel;
+@property (nonatomic, strong, readonly) JSTDetailsHeaderView *headerView;
 @end
