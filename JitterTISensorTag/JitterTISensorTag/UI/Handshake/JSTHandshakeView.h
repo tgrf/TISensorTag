2319bcbd20676fc65523916f1ee8b92215865350
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2015-02-03 19:44:03 +0100

    teddy bear;

--- JitterTISensorTag/JitterTISensorTag/UI/Handshake/JSTHandshakeView.h
+++ JitterTISensorTag/JitterTISensorTag/UI/Handshake/JSTHandshakeView.h
@@ -7,6 +7,12 @@
 //
 #import <Foundation/Foundation.h>
 
+@class JSTDetailsFooterView;
+@class JSTDetailsResultView;
+@class JSTDetailsHeaderView;
+
 @interface JSTHandshakeView : UIView
-@property (nonatomic, strong, readonly) UILabel *valuesLabel;
+@property (nonatomic, strong, readonly) JSTDetailsHeaderView *headerView;
+@property (nonatomic, strong, readonly) JSTDetailsResultView *resultView;
+@property (nonatomic, strong, readonly) JSTDetailsFooterView *footerView;
 @end
--- JitterTISensorTag/JitterTISensorTag/UI/Handshake/JSTHandshakeView.m
+++ JitterTISensorTag/JitterTISensorTag/UI/Handshake/JSTHandshakeView.m
@@ -7,31 +7,57 @@
 //
 #import "JSTHandshakeView.h"
 #import "View+MASAdditions.h"
+#import "JSTDetailsFooterView.h"
+#import "JSTDetailsResultView.h"
+#import "JSTDetailsHeaderView.h"
+#import "UIColor+JSTExtensions.h"
 
 @implementation JSTHandshakeView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor whiteColor];
+        self.backgroundColor = [UIColor defaultJSTColor];
 
-        _valuesLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-        self.valuesLabel.font = [UIFont systemFontOfSize:30];
-        self.valuesLabel.textAlignment = NSTextAlignmentCenter;
-        self.valuesLabel.numberOfLines = 0;
-        [self addSubview:self.valuesLabel];
+        _headerView = [[JSTDetailsHeaderView alloc] initWithFrame:CGRectZero];
+        _headerView.iconView.text = @"P";
+        _headerView.titleLabel.text = @"Teddy bear";
+        _headerView.descriptionLabel.text = @"Say hello to teddy bear! Don't you want to make a new friend?";
+        [self addSubview:_headerView];
 
-        [self updateConstraints];
+        _resultView = [[JSTDetailsResultView alloc] initWithFrame:CGRectZero];
+        _resultView.resultLabel.text = @"0%";
+        [self addSubview:_resultView];
+
+        _footerView = [[JSTDetailsFooterView alloc] initWithFrame:CGRectZero];
+        [self addSubview:_footerView];
+
+        [self setNeedsUpdateConstraints];
+        [self updateConstraintsIfNeeded];
     }
 
     return self;
 }
 
 - (void)updateConstraints {
-    [_valuesLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.mas_top).with.offset(64);
+    [super updateConstraints];
+
+    [_headerView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top);
         make.left.equalTo(self.mas_left);
         make.right.equalTo(self.mas_right);
+    }];
+
+    [_resultView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.footerView.mas_top);
+        make.centerX.equalTo(self.mas_centerX);
+        make.left.equalTo(self.mas_left);
+        make.right.equalTo(self.mas_right);
+    }];
+
+    [_footerView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.mas_bottom);
+        make.centerX.equalTo(self.mas_centerX);
     }];
 
     [super updateConstraints];
